5dbeffadb285c44cf83587919b4561f89ac566be
mesquini <mesquini@live.com>
2020-01-31 21:30:15 -0300

    add jwt, yup, bcryptjs

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -1,19 +1,33 @@
-import { Model, Sequelize } from 'sequelize'
+import { Model, Sequelize } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
-  static init (sequelize) {
+  static init(sequelize) {
     super.init(
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
-        provider: Sequelize.BOOLEAN
+        provider: Sequelize.BOOLEAN,
       },
       {
-        sequelize
+        sequelize,
       }
-    )
+    );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
-export default User
+export default User;
---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '5117605dd7309ffb15b57768e711643a',
+  expires: '7d'
+}
