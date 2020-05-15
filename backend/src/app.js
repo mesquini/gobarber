61acabf57d5ea6c532e9973643ceec3874fbfa25
victor.mesquini <victor.mesquini@ingaia.com.br>
2020-05-15 14:22:14 -0300

    add seguraça at backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,9 @@ import path from 'path';
 import Youch from 'youch';
 import cors from 'cors';
 import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import morgan from 'morgan';
 import * as Sentry from '@sentry/node';
 import sentryConfig from './config/sentry';
@@ -28,12 +31,31 @@ class App {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(morgan('dev'));
     this.server.use(helmet());
-    this.server.use(cors());
+    this.server.use(
+      cors({
+        origin: process.env.FRONT_URL,
+      })
+    );
     this.server.use(express.json());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
